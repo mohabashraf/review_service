28d82d642786fe3b2b2887d6f237fd8cd6bc6313
mohabashraf <mohabashraf466@gmail.com>
2020-02-25 21:14:33 +0200

    initial

---
+++ api/src/test/java/com/review/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

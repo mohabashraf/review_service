40ad2f608a71f45a5f8699b9397a61038f0fef62
mohabashraf <mohabashraf466@gmail.com>
2020-02-25 21:50:16 +0200

    initial

--- review-service/src/main/java/ReviewServiceApplication.java
+++ review-service/src/main/java/ReviewServiceApplication.java
@@ -1,13 +1,10 @@
-package com.review.reviewservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReviewServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ReviewServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewServiceApplication.class, args);
+    }
 
 }
